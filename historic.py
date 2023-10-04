fe5314db81c7a79101fb89dff389c28c55b0b8d3
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-10-04 16:20:00 -0300

    Update historic.py
    Documentation

--- historic.py
+++ historic.py
@@ -1,45 +1,38 @@
+""" Module for handling historic for the program's many functions """
 import datetime
-# Appends price to the price historic list, is called in the alter price function #
 def edit_price_historic(product_stock,name,new_price):
+    """ Appends price to the price historic list, called in the alter price function """
     product_stock[name]["price_historic"].append(new_price)
     return product_stock
 
-# Every edit changelog function appends a message to a list of changes #
-# Called when adding a new product #
 def edit_changelog_add(name,amount,price,category,changelog):
+    """ Called when adding a new product, appends a message to a list of changes """
     changelog.append(f"Product {name} added to stock; amount {amount}; price {price}; category {category}.")
     return changelog
 
-# Called when adding an amount to a product #
 def edit_changelog_amount(name,add_amount,product_stock,changelog):
+    """ Called when adding an amount to a product, appends a message to a list of changes """
     changelog.append(f"Amount {add_amount} added to product {name}, new total amount {product_stock[name]['amount']}.")
     return changelog
 
-# Called when altering a product's price #
 def edit_changelog_alter(name,product_stock,new_price,changelog):
+    """ Called when altering a product's price, appends a message to a list of changes """
     changelog.append(f"Product {name} price altered from {product_stock[name]['price']} to {new_price}.")
     return changelog
 
-# Called when deleting a product #
 def edit_changelog_exclusion(exclusion,changelog):
+    """ Called when deleting a product, appends a message to a list of changes """
     changelog.append(f"Product {exclusion} deleted from stock.")
     return changelog
 
-# Shows messages through a for loop #
 def view_changelog(changelog):
+    """ Shows change messages through a for loop """
     for change in changelog:
         print(change)
     enter = input("Press enter to go back to the menu...")
 
-# Used to get sale information in the search a product function #
-def filter_sales_report(name,sales_report,product_sales):
-    for key, value in sales_report.items():
-        if value.get("codename") == name:
-            product_sales[key] = value
-    return product_sales
-
-# Adds sale information to the sales report #
 def edit_sales_report(sales_report,sale_count,name,amount_sold,profit):
+    """ Adds sale information to sales report, called in the sales module """
     sale_count += 1
     sales_key = str(sale_count) + ". " + name
     sales_report[sales_key] = {
@@ -50,8 +43,8 @@ def edit_sales_report(sales_report,sale_count,name,amount_sold,profit):
     }
     return sales_report,sale_count
 
-# Shows sales information through a for loop #
 def view_sales_report(sales_report):
+    """ Shows sales information through a for loop """
     if len(sales_report) == 0:
         print("No products were sold.")
     else:
